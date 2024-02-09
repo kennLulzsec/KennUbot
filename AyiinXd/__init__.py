2239033f00281f44dbae401d44d0e0a740ffebce
kennLulzsec <159522056+kennLulzsec@users.noreply.github.com>
2024-02-09 15:26:27 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -41,7 +41,7 @@ var = Config()
 if var.STRING_SESSION:
     session = validate_session(var.STRING_SESSION)
 else:
-    session = "ObyUserBot"
+    session = "KennUserBot"
 try:
     Ayiin = TelegramClient(
         session=session,
